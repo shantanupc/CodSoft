dc95bb4aefcd52b763b4246cb54b54c3c44e72f3
ShantanuPC <shantanuchimote42@gmail.com>
2024-10-20 01:33:26 +0530

    Upload Task 2 Program
    Signed-off-by: ShantanuPC <shantanuchimote42@gmail.com>

---
+++ Task_2_Student_Grade_Calculator.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class StudentGradeCalculator {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter the number of subjects: ");
+        int numSubjects = scanner.nextInt();
+        
+        int[] marks = new int[numSubjects];
+        int totalMarks = 0;
+        
+        for (int i = 0; i < numSubjects; i++) {
+            System.out.print("Enter marks obtained in subject " + (i + 1) + " (out of 100): ");
+            marks[i] = scanner.nextInt();
+            totalMarks += marks[i];
+        }
+        
+        double averagePercentage = (double) totalMarks / numSubjects;
+
+        String grade;
+        if (averagePercentage >= 90) {
+            grade = "A";
+        } else if (averagePercentage >= 80) {
+            grade = "B";
+        } else if (averagePercentage >= 60) {
+            grade = "C";
+        } else if (averagePercentage >= 40) {
+            grade = "D";
+        } else {
+            grade = "F";
+        }
+
+        System.out.println("\n--- Result ---");
+        System.out.println("Total Marks: " + totalMarks + " out of " + (numSubjects * 100));
+        System.out.printf("Average Percentage: %.2f%%\n", averagePercentage);
+        System.out.println("Grade: " + grade);
+
+        scanner.close();
+    }
+}
